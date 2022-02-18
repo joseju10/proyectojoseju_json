c68716f6daa88fd43f5f241bb1c8afcb5338f53b
joseju10 <58250763+joseju10@users.noreply.github.com>
2022-02-18 10:47:36 +0100

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,8 @@
+def listar_informacion():
+    #numero de equipos
+def contar_informacion():
+    #Mostrar la cantidad de partidos en los que un equipo ha marcado mas de un gol.
+def buscar_informacion():
+    #mostrar partido jugado de la jornada 7 que haya habido minimo un gol por partido. 
+def informacion_relacionada():
+    #pide por teclado la informacion relacionada de un equipo y mostramos el nombre del entrenador, el nombre del estadio y el año de fundacion.
